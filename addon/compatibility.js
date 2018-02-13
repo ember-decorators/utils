3a5cf624eb7f74e31bf0036e31c42961bd4de2b9
Christopher Garrett <me@pzuraq.com>
2018-02-12 21:48:51 -0800

    [bugfix] Fixes property definitions and computed helper
    1. Computed properties depend on being writable properties (specifically
    macros like `oneWay` and `reads`).
    
    2. Computed helper didn't accept dependent keys

--- addon/compatibility.js
+++ addon/compatibility.js
@@ -2,18 +2,30 @@ import { computed as emberComputed } from '@ember/object';
 
 import { SUPPORTS_NEW_COMPUTED } from 'ember-compatibility-helpers';
 
-export function computed(desc) {
-  if (SUPPORTS_NEW_COMPUTED || typeof desc === 'function') {
-    return emberComputed(desc);
-  } else {
-    const { get, set } = desc;
+let computed;
 
-    return emberComputed(function (key, value) {
-      if (arguments.length > 1) {
-        return set.call(this, key, value);
-      }
+if (SUPPORTS_NEW_COMPUTED) {
+  computed = emberComputed;
+} else {
+  computed = function(...params) {
+    let desc = params.pop();
 
-      return get.call(this);
-    });
-  }
+    if (typeof desc === 'function') {
+      return emberComputed(...params, desc);
+    } else if ('set' in desc) {
+      const { get, set } = desc;
+
+      return emberComputed(...params, function (key, value) {
+        if (arguments.length > 1) {
+          return set.call(this, key, value);
+        }
+
+        return get.call(this);
+      });
+    } else {
+      return emberComputed(...params, desc.get);
+    }
+  };
 }
+
+export { computed };
